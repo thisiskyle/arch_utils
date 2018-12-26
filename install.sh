4ca8f3d20357c772f7947f5ad5271894af3952be
kyle <email@email.com>
2018-12-26 18:43:44 -0500

    updates to install and save scripts

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cp ./.Xresources $HOME/.Xresources2
+cp ./.Xresources $HOME/.Xresources
 
-cp ./.bashrc $HOME/.bashrc2
+cp ./.bashrc $HOME/.bashrc
 
-cp ./.fehbg $HOME/.fehbg2
+cp ./.fehbg $HOME/.fehbg
 
-cp ./.xinitrc $HOME/.xinitrc2
+cp ./.xinitrc $HOME/.xinitrc
 
-cp -r ./.config $HOME/.config2
+cp -r ./.config $HOME/.config
 
---
+++ save.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp $HOME/.Xresources ./.Xresources 
+
+cp $HOME/.bashrc ./.bashrc
+
+cp $HOME/.fehbg ./.fehbg
+
+cp $HOME/.xinitrc ./.xinitrc
+
+cp -r $HOME/.config ./.config
+
