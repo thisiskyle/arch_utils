38079995473dc8fbca769a02803111ef3179999f
kyle <email@email.com>
2018-12-31 23:46:03 -0500

    added wallpaper folder

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 cp ./.Xresources $HOME/.Xresources
-
 cp ./.bashrc $HOME/.bashrc
-
 cp ./.fehbg $HOME/.fehbg
-
 cp ./.xinitrc $HOME/.xinitrc
-
 cp -r ./.config $HOME/.config
+cp -r ./wallpaper $HOME/wallpaper
 
--- save.sh
+++ save.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 cp $HOME/.Xresources ./.Xresources 
-
 cp $HOME/.bashrc ./.bashrc
-
 cp $HOME/.fehbg ./.fehbg
-
 cp $HOME/.xinitrc ./.xinitrc
-
 cp -r $HOME/.config ./.config
+cp -r $HOME/wallpaper ./wallpaper
 
