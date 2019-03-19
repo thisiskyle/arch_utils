ce57e5d128763de0d0c939e57b2c3fec4fdb0b72
Kyle Ericson <Kyle.ericson2@gmail.com>
2019-03-19 14:27:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cp $HOME/.dotfiles/.Xdefaults $HOME
 cp $HOME/.dotfiles/.bashrc $HOME
 cp $HOME/.dotfiles/.fehbg $HOME
 cp $HOME/.dotfiles/.xinitrc $HOME
-cp -R $HOME/.dotfiles/.config $HOME
+cp $HOME/.dotfiles/.config/i3/config $HOME/.config/i3
+cp $HOME/.dotfiles/.config/i3status/config $HOME/.config/i3status
 cp -R $HOME/.dotfiles/wallpaper $HOME
 
