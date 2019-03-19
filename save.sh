4b9aba0b1c446ab746cf90e61b5fbaf89070a276
Kyle Ericson <Kyle.ericson2@gmail.com>
2019-03-19 14:30:15 -0400

    Update save.sh

--- save.sh
+++ save.sh
@@ -4,6 +4,9 @@ cp $HOME/.Xdefaults $HOME/.dotfiles
 cp $HOME/.bashrc $HOME/.dotfiles
 cp $HOME/.fehbg $HOME/.dotfiles
 cp $HOME/.xinitrc $HOME/.dotfiles
-cp -R $HOME/.config $HOME/.dotfiles
-cp -R $HOME/wallpaper $HOME/.dotfiles
 
+cp $HOME/.config/i3/config $HOME/.dotfiles/.config/i3
+cp $HOME/.config/i3status/config $HOME/.dotfiles/.config/i3status
+cp $HOME/.config/compton.conf $HOME/.dotfiles/.config/compton.conf
+
+cp -R $HOME/wallpaper $HOME/.dotfiles
