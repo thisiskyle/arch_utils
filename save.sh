5dcba15fff771805a505b9448522ae5f0fa63543
Kyle <email@email.com>
2019-04-19 16:34:05 +0100

    added .profile

--- save.sh
+++ save.sh
@@ -4,6 +4,7 @@ cp $HOME/.Xdefaults $HOME/.dotfiles
 cp $HOME/.bashrc $HOME/.dotfiles
 cp $HOME/.fehbg $HOME/.dotfiles
 cp $HOME/.xinitrc $HOME/.dotfiles
+cp $HOME/.profile $HOME/.dotfiles
 
 cp $HOME/.config/i3/config $HOME/.dotfiles/.config/i3
 cp $HOME/.config/i3status/config $HOME/.dotfiles/.config/i3status
