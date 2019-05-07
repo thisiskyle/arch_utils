bfd11239a1985e3ee2a986162e0e4b921b533fcf
Kyle Ericson <Kyle.ericson2@gmail.com>
2019-05-07 10:30:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,5 @@ cp $HOME/.dotfiles/.profile $HOME
 cp $HOME/.dotfiles/.config/i3/config $HOME/.config/i3
 cp $HOME/.dotfiles/.config/i3status/config $HOME/.config/i3status
 
-cp $HOME/.dotfiles/.config/compton.conf $HOME/.config/compton.conf
-
 cp -R $HOME/.dotfiles/wallpaper $HOME
 
