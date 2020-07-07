bc14c66d114940622524c09454b9e6f09d3d9107
kyle <kyle.ericson1@gmail.com>
2020-07-07 12:00:28 -0400

    some tweaks

--- scripts/set_res.sh
+++ scripts/set_res.sh
@@ -4,3 +4,5 @@ xrandr --newmode "1920x1080_60.00" 172.80 1920 2040 2248 2576 1080 1081 1084 111
 xrandr --addmode Virtual-1 "1920x1080_60.00"
 xrandr --output Virtual-1 --mode "1920x1080_60.00"
 
+feh --bg-scale ~/.dotfiles/wallpaper/outerwilds.jpg
+
