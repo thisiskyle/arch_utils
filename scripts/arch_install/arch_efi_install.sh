3930d47c82552a6907b16d4c46a59a43709d1885
Kyle Ericson <kyle.ericson2@gmail.com>
2022-08-18 22:13:16 -0400

    adding some of the more complicated arch-chroot stuff

--- scripts/arch_install/arch_efi_install.sh
+++ scripts/arch_install/arch_efi_install.sh
@@ -2,9 +2,9 @@
 
 # this function asks the user for a partition number,
 # verfies that the value is numerical, then returns it or asks again
-ask_for_partition_num() {
+ask_for_num() {
     reg="^[0-9]+$"
-    echo -n "Enter partition number: " >&2
+    echo -n "Enter number: " >&2
     read n
     while ! [[ $n =~ $reg ]]; do
         echo -n "Response must be a number: " >&2
@@ -12,6 +12,10 @@ ask_for_partition_num() {
     done
     echo $n
 }
+
+
+archChrootScript="/mnt/root/temp.sh"
+
 
 # set the time protocol
 echo "Setting time protocol sync"
@@ -40,11 +44,11 @@ echo "Assuming this is a simple EFI install and based on the fdisk command file 
 
 # ask the user for efi partition number
 echo "What partition will be EFI?"
-efiPartitionNum=$(ask_for_partition_num)
+efiPartitionNum=$(ask_for_num)
 
 # ask user for root partition number
 echo "What partition will be root?"
-rootPartitionNum=$(ask_for_partition_num)
+rootPartitionNum=$(ask_for_num)
 
 # set the full dev partition paths
 efiPart="${targetDev}${efiPartitionNum}"
@@ -89,15 +93,42 @@ mount "${efiPart}" /mnt/boot/efi
 
 # install arch
 echo "Installing Arch with pacstrap"
-pacstrap /mnt base linux linux-firmware vim dhcpcd
+pacstrap /mnt base linux linux-firmware vim dhcpcd base-devel
 
 # generate fstab
 echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
+echo "How large do you want the swapfile? (in GB)"
+swap_size=$(ask_for_num)
+
+swap_path="/swapfile"
+
 
 # TODO/incomplete: arch-chroot stuff here use a cat heredoc or whatever to create a file on the /mnt parition
+cat <<EOF > ${archChrootScript}
 
+dd if=/dev/zero of=${swap_path} bs=1G count=${swap_size} status=progress
+chmod 600 ${swap_path}
+mkswap ${swap_path}
+swapon ${swap_path}
+echo "${swap_path}  none  swap  defaults  0 0" >> /etc/fstab
+
+ln -sf /usr/share/zoneinfo/America/Detroit /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+export LANG=en_US.UTF-8
+
+
+
+locale-gen
+
+
+EOF
+
+
+arch-chroot /mnt "${archChrootScript}"
 
 
 
