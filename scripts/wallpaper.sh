1675ad097d5c3720addccdd9e0534f17de6933c1
Kyle Ericson <kyle.ericson@gentex.com>
2022-10-06 13:12:24 -0400

    more wallpaper start up stuff

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-if [[ ! -f "${1}" ]]; then
-    image_path="$HOME/arch_utils/wallpaper/${1}"
-else
-    image_path="${1}"
-fi
+checks=(
+    "${1}"
+    "$HOME/arch_utils/wallpaper/${1}"
+)
+
+for i in "${checks[@]}"
+do
+    if [[ -f "${i}" ]]; then
+        image_path="${i}"
+    fi
+done
+
 
 if [[ ! -f "${image_path}" ]]; then
-    echo "ERROR: image could not be found at ${1} or ${image_path}"
+    echo "ERROR: image could not be found"
     exit 1
 fi
 
 echo "WALLPAPER=\"${image_path}\"" > "$HOME/.wallpaper"
-feh --bg-scale "${image_path}" &
+
+pgrep 'startx' && feh --bg-scale "${image_path}" &
