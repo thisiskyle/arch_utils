be0f206a8376cb922a5405172f8daffacb221209
Kyle <>
2023-05-03 20:50:30 -0400

    added a list option to the wallpaper script

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
-checks=(
-    "${1}"
-    "$HOME/arch_utils/wallpaper/${1}"
+
+
+wallpaper_locations=(
+    "$HOME/arch_utils/wallpaper/"
 )
 
-for i in "${checks[@]}"
+
+if [[ "${1}" == "-list" ]]; then
+
+    for i in "${wallpaper_locations[@]}"
+    do
+        for e in "${i}"*
+        do
+            echo "$(basename -- $e)"
+        done
+    done
+
+    exit
+fi
+
+
+
+
+for i in "${wallpaper_locations[@]}"
 do
-    if [[ -f "${i}" ]]; then
-        image_path="${i}"
+    if [[ -f "${i}${1}" ]]; then
+        image_path="${i}${1}"
     fi
 done
+
 
 if [[ ! -f "${image_path}" ]]; then
     echo "ERROR: image could not be found"
     exit 1
 fi
+
 
 feh --bg-scale "${image_path}" &
